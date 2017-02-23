bdeca9ff1d1673da9856af6069994013cb51b588
anurag1192 <anurag.cav@gmail.com>
2017-02-23 17:09:09 -0600

    Update max_after_removing2adjnumbers.py

--- max_after_removing2adjnumbers.py
+++ max_after_removing2adjnumbers.py
@@ -4,9 +4,10 @@ b = list()
 for i in range(len(a)-1):
     if a[i]==a[i+1]:
         b.append("".join(a[:i]+a[i+1:]))
-print b
-max = 0
-for i in range(len(b)):
-    if b[i]>max:
-        max = b[i]
-print max
+#print b
+#max = 0
+#for i in range(len(b)):
+#    if b[i]>max:
+#        max = b[i]
+#print max
+print max(b)
